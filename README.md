fb9c9205c98a7e3f0c0574de3cd83c3230cb7b93
mayank <sismmt09@gmail.com>
2021-01-17 21:58:55 +0530

    converted url_extractor to a module

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # scrapper
 
+## Requirements
+- requests
+- beautifullsoup
+
+# Steps to install
+- pip install requests
+- pip install beautifulsoup4
+
 ## To test the scrapper following sites are used
 - https://www.webscraper.io/test-sites
--- url_extractor.py
+++ url_extractor.py
@@ -1,23 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.webscraper.io/test-sites/e-commerce/allinone"
 
-response = requests.get(url)
-print(response)         # <Response [200]> if it was a successfull request.
+def extract(url):
+    # url = "https://www.webscraper.io/test-sites/e-commerce/allinone"
 
-source_code = response.text
-# print(source_code)
+    response = requests.get(url)
+    print(response)         # <Response [200]> if it was a successfull request.
 
-data = BeautifulSoup(source_code, 'html.parser')
-# print(data)
+    source_code = response.text
+    # print(source_code)        # raw source code of the website
 
-# finding html tags
-# a tags
-tags = data.find_all('a')
-# print(tags)         # returns all the anchor tags
+    data = BeautifulSoup(source_code, 'html.parser')
+    # print(data)               # parsed source code
 
-# find links in page.
+    # finding html tags
+    # a tags
+    # tags = data.find_all('a')
+    # print(tags)         # returns all the anchor tags
 
-for tag in tags:
-    print(tag.get('href'))              # prints all the links in the page.
+    # find links in page.
+
+    # for tag in tags:
+        # print(tag.get('href'))              # prints all the links in the page.
+    
+    # return a beautifulsoup object
+    return data
